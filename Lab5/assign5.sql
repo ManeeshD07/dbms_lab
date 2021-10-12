16825c3686f6f603f4cf698307d8c3ada7a71da7
ManeeshD07 <58357149+ManeeshD07@users.noreply.github.com>
2021-10-12 14:58:26 +0530

    Update assign5.sql

--- Lab5/assign5.sql
+++ Lab5/assign5.sql
@@ -1,3 +1,4 @@
+/*--------------------1------------------*/
 create table marks_sem1 as select * from student;
 
 alter table marks_sem1 
@@ -18,8 +19,7 @@ drop column team;
 alter table marks_sem1
 add primary key(rollno);
 
-show columns from marks_sem1;
-
+/*-------------------2-----------------*/
 create table faculty(name varchar(20), fid char(4), subject varchar(20));
 insert into faculty(name, fid, subject) values('kakashi', 4003, 'math'); 
 insert into faculty(name, fid, subject) values('guy', 5002, 'sports'); 
@@ -28,4 +28,105 @@ insert into faculty(name, fid, subject) values('kurenai', 5038, 'eng');
 alter table faculty
 add primary key(fid);
 
-show columns from faculty;
+/*-----------------3---------------*/
+create table sem1(subj_id char(4), subject varchar(20), fid char(4), cap integer, campus integer);
+insert into sem1(subj_id,subject,fid,cap,campus) values(101, 'math', 4003, 60, 101);
+insert into sem1(subj_id,subject,fid,cap,campus) values(102, 'sports', 5002, 60, 101);
+insert into sem1(subj_id,subject,fid,cap,campus) values(105, 'eng', 5038, 60, 104);
+
+alter table sem1
+add primary key(subj_id);
+
+alter table sem1
+add key(fid);
+
+alter table sem1
+add key(campus);
+
+
+/*---------------4------------*/
+alter table campus
+add primary key(cid);
+
+/*--------------5--------------*/
+alter table student
+add column cid integer;
+
+update student
+set cid = 109
+where name = 'dwayne';
+
+update student
+set cid = 101
+where name = 'john';
+
+update student
+set cid = 110
+where name = 'dave';
+
+update student
+set cid = 104
+where name = 'randy';
+
+update student
+set cid = 104
+where name = 'kane';
+
+update student
+set cid = 101
+where name = 'tom';
+
+update student
+set cid = 113
+where name = 'carol';
+
+update student
+set cid = 107
+where name = 'wanda';
+
+update student
+set cid = 109
+where name = 'natasha';
+
+update student
+set cid = 109
+where name = 'gamora';
+
+update student
+set cid = 110
+where name = 'jean';
+
+alter table student
+drop column math;
+
+alter table student
+drop column sci;
+
+alter table student
+drop column eng;
+
+alter table student
+drop column social;
+
+alter table student
+drop column sports;
+
+alter table student
+add foreign key(cid) references campus(cid);
+
+alter table student
+add foreign key (rollno) references marks_sem1(rollno);
+
+alter table student
+alter dob set default '2001-01-01';
+
+alter table student
+alter yjoin set default 2019;
+
+alter table student
+alter gender set default 'M';
+
+alter table student
+alter team set default 'R';
+
+describe student;
